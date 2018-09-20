993ffc8ad7cad6bd61bf6ace17cce3a5be3d9528
Michał Chudziak <michal.chudziak@gmail.com>
2018-09-20 15:27:37 +0200

    new version

--- applesimutils.rb
+++ applesimutils.rb
@@ -2,7 +2,7 @@ class Applesimutils < Formula
   desc 'Apple simulator utilities'
   homepage 'https://github.com/michalchudziak/AppleSimulatorUtils'
   url 'https://raw.githubusercontent.com/michalchudziak/homebrew-brew/master/AppleSimulatorUtils-0.5.25.tar.gz'
-  sha256 '38b71d55eaaca6016c6beffe1e058ed10e8af6da5fffce064e4edb3eaa369f51'
+  sha256 '1c8aa99763e82cbe06ab8fb2b1b1b063e68ebe88a9404ec1ce26e53a6d08bd43'
   head 'https://github.com/michalchudziak/AppleSimulatorUtils.git'
 
   depends_on xcode: ['10.0', :build]
