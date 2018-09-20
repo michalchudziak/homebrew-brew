18455e44fe014ffbe44b959b422bffb96258eea5
Michał Chudziak <michal.chudziak@gmail.com>
2018-09-20 15:43:22 +0200

    fix the sha

--- applesimutils.rb
+++ applesimutils.rb
@@ -1,7 +1,7 @@
 class Applesimutils < Formula
   desc 'Apple simulator utilities'
   homepage 'https://github.com/michalchudziak/AppleSimulatorUtils'
-  url 'https://raw.githubusercontent.com/michalchudziak/homebrew-brew/master/AppleSimulatorUtils-0.5.25.tar.gz'
+  url 'https://raw.githubusercontent.com/michalchudziak/homebrew-brew/master/AppleSimulatorUtils-0.5.26.tar.gz'
   sha256 '01c2491266124dd136063b87d334cc0493442d58d6de9056b6cca465c7119afe'
   head 'https://github.com/michalchudziak/AppleSimulatorUtils.git'
 
